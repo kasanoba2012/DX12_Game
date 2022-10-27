984b099ddbab4b1c2574b3d93264cb0f1ca6574d
Minsuhong <kasanoba2012@naver.com>
2022-10-27 18:12:30 +0900

    삼각형 그리기

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -2,12 +2,39 @@
 #include "Game.h"
 #include "Engine.h"
 
+shared_ptr<Mesh> mesh = make_shared<Mesh>();
+shared_ptr<Shader> shader = make_shared<Shader>();
+
 void Game::Init(const WindowInfo& info)
 {
 	GEngine->Init(info);
+	
+	// 삼각형 정보 만들기
+	vector<Vertex> vec(3);
+	vec[0].pos = Vec3(0, 0.5f, 0.5f);
+	vec[0].color = Vec4(1.f, 0, 0, 1.f);
+	vec[1].pos = Vec3(0.5f, -0.5f, 0.5f);
+	vec[1].color = Vec4(0, 1, 0, 1.f);
+	vec[2].pos = Vec3(-0.5f, -0.5f, 0.5f);
+	vec[2].color = Vec4(0, 0, 1, 1.f);
+
+	// 해당 삼각형 정보를 메쉬 init를 통해서 GPU 메모리쪽으로 전달
+	mesh->Init(vec);
+
+	// 쉐이더 파일을 읽어서 어떻게 쉐이더 처리할지 컴파일
+	shader->Init(L"..\\Resources\\Shader\\default.hlsli");
+
+	GEngine->GetCmdQueue()->WaitSync();
 }
 
 void Game::Update()
 {
-	GEngine->Render();
+	GEngine->RenderBegin();
+
+	// 쉐이더 파일을 읽어서 그림그려달라는 대로 그리기 준비
+	shader->Update();
+	// 무엇을 그려달라는지 확인하고 물체 그려달라는 명령어 전달
+	mesh->Render();
+
+	GEngine->RenderEnd();
 }
--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -1,8 +1,5 @@
 #include "pch.h"
 #include "Engine.h"
-#include "Device.h"
-#include "CommandQueue.h"
-#include "SwapChain.h"
 
 void Engine::Init(const WindowInfo& info)
 {
@@ -16,10 +13,12 @@ void Engine::Init(const WindowInfo& info)
 	_device = make_shared<Device>();
 	_cmdQueue = make_shared<CommandQueue>();
 	_swapChain = make_shared<SwapChain>();
+	_rootSignature = make_shared<RootSignature>();
 
 	_device->Init();
 	_cmdQueue->Init(_device->GetDevice(), _swapChain);
 	_swapChain->Init(info, _device->GetDevice(), _device->GetDXGI(), _cmdQueue->GetCmdQueue());
+	_rootSignature->Init(_device->GetDevice());
 }
 
 void Engine::Render()
