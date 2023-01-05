11928661b0eec2725769b362adec772d90bfed73
Minsuhong <kasanoba2012@naver.com>
2023-01-05 16:46:27 +0900

    멀티쓰레드 Atomic (공유 값 컨트롤)

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,8 +1,37 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <thread>
+#include <atomic>
+
+atomic<int32> sum = 0;
+
+void Add()
+{
+	for(int32 i = 0; i < 100'000; i++)
+	{
+		sum.fetch_add(1);
+	}
+}
+
+void Sub()
+{
+	for (int32 i = 0; i < 100'000; i++)
+	{
+		sum.fetch_add(-1);
+	}
+}
 
 int main()
 {
-	HelloWorld();
+	Add();
+	Sub();
+	cout << sum << endl;
+
+	std::thread t1(Add);
+	std::thread t2(Sub);
+	t1.join();
+	t2.join();
+
+	cout << sum << endl;
 }
