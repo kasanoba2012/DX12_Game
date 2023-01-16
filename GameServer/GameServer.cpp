7781e4c094158ae19d103c32f26381a33d651156
Minsuhong <kasanoba2012@naver.com>
2023-01-16 18:17:57 +0900

    [게임서버] Reference Counting

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,74 +8,80 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include <vector>
-#include <thread>
-
-// 소수 구하기
-bool IsPrime(int number)
-{
-	// 1이거나 1보다 작으면 끝
-	if (number <= 1)
-		return false;
-	// 2거나 3면 소수
-	if (number == 2 || number == 3)
-		return true;
-	// 1 2 3 이 아닌 그외 숫자들
-	for (int i = 2; i < number; i++)
-	{
-		if ((number % i) == 0)
-			return false;
-	}
-	// 나눠지는 수가 없으면 소수
-	return true;
-}
-
-// [start ~ end]
-int CountPrime(int start, int end)
-{
-	int count = 0;
-	for (int number = start; number <= end; number++)
-	{
-		if (IsPrime(number))
-			count++;
-	}
-	return count;
-}
+#include "RefCounting.h"
 
 using namespace std;
 
-// 1과 자기 자신으로만 나뉘면 그것을 소수라고 함
+class Wraight : public RefCountable
+{
+public:
+	int _hp = 150;
+	int _posX = 0;
+	int _posY = 0;
+};
+
+using WraightRef = TSharedPtr<Wraight>;
+
+class Missile : public RefCountable
+{
+public:
+	void SetTarget(WraightRef target)
+	{
+		_target = target;
+		// 중간에 개입 가능
+		// target->AddRef();
+	}
+
+	bool Update()
+	{
+		if (_target == nullptr)
+			return true;
+
+		int posX = _target->_posX;
+		int posY = _target->_posY;
+
+		// TODO:쫓아간다
+
+		if (_target->_hp == 0)
+		{
+			_target = nullptr;
+			return true;
+		}
+		return false;
+	}
+	WraightRef _target = nullptr;
+};
+
+using MissileRef = TSharedPtr<Missile>;
 
 int main()
 {
-	const int MAX_NUMBER = 100'0000;
-	// 1~MAX_NUMBER까지의 소수 개수
-	vector<thread> threads;
+	WraightRef wraight(new Wraight());
+	wraight->ReleaseRef();
+	MissileRef missile(new Missile());
+	missile->ReleaseRef();
 
-	// 1000 = 168
-	// 1'0000 = 1229
-	// 100'0000 = 78498
-	
-	// hardware_concurrency() : CPU 하나에, 코어가 4개 달린 CPU를 사용중이라면 결과는 4가 나온다.
-	int coreCount = thread::hardware_concurrency();
-	// [(하드웨어 스레드 컨텍스트 수 * 2) + 1]만큼을 사용하면 효율이 좋다고 한다.
-	int jobCount = (MAX_NUMBER / coreCount) + 1;
+	missile->SetTarget(wraight);
 
-	atomic<int> primeCount = 0;
+	// 레이스가 피격 당함
+	wraight->_hp = 0;
+	//delete wraight;
+	//wraight->ReleaseRef();
+	wraight = nullptr;
 
-	for (int i = 0; i < coreCount; i++)
+	while (true)
 	{
-		int start = (i * jobCount) + 1;
-		int end = min(MAX_NUMBER, ((i + 1) * jobCount));
-
-		threads.push_back(thread([start, end, &primeCount]()
+		if (missile)
+		{
+			if (missile->Update())
 			{
-				primeCount += CountPrime(start, end);
-			}));
+				//missile->ReleaseRef();
+				missile = nullptr;
+			}
+		}
 	}
 
-	for (thread& t : threads)
-		t.join();
-
-	cout << primeCount << endl;
+	//missile->ReleaseRef();
+	missile = nullptr;
+	//delete missile;
 }
